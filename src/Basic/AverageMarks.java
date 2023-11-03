f25cbe94af8f02782f088580647243745db8df1c
shaheer-alam-bit <s.alam.25144@khi.iba.edu.pk>
2023-11-03 20:08:23 +0500

    Deleted Printing marks line Added a print line for taking input

--- src/Basic/AverageMarks.java
+++ src/Basic/AverageMarks.java
@@ -3,21 +3,24 @@ import java.util.*;
 public class AverageMarks {
 public static void main(String[] args) {
 		
+		System.out.println("Enter the name of the student and marks of three subjects");
         Scanner sc =new Scanner(System.in);
 			
 	        String s= sc.next();
 		        int m1= sc.nextInt();
 		        int m2= sc.nextInt();
 		        int m3= sc.nextInt();
-        		     int marks = (m1+m2+m3)/3;              
+        		     int marks = (m1+m2+m3)/3;   
+		       
 System.out.println(s);
-	                 System.out.print(marks);
+sc.close();    
+	                 
 	                 System.out.println("Master 2.0 By Shaheer Alam 25144");
+					
 // 	                 System.out.print(m2+" " );
 // 	                 System.out.print(m3);
 		        
 		        //marks = m1+m2+m3/100;
 		        // System.out.println(marks);
 	}
-
 }
