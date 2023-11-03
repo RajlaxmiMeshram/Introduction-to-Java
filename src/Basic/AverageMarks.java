d94511459d6ee5d0eea206a727f3df56ab30add0
shaheer-alam-bit <s.alam.25144@khi.iba.edu.pk>
2023-11-03 21:11:09 +0500

    Added a line to take input from user Added a line at the end to output student name and erp along with marks

--- src/Basic/AverageMarks.java
+++ src/Basic/AverageMarks.java
@@ -2,7 +2,7 @@ package Basic;
 import java.util.*;
 public class AverageMarks {
 public static void main(String[] args) {
-		
+		System.out.println("Enter marks for 3 subjects:");
         Scanner sc =new Scanner(System.in);
 			
 	        String s= sc.next();
@@ -12,6 +12,7 @@ public static void main(String[] args) {
         		     int marks = (m1+m2+m3)/3;              
 System.out.println(s);
 	                 System.out.print(marks);
+					 System.out.println("Shaheer Alam 25144 scored" , marks," average marks in 3 subjects");
 	                 
 // 	                 System.out.print(m2+" " );
 // 	                 System.out.print(m3);
